653aa9955759d65444318ef4a6a55fa40d0ce4aa
Harout Bezdjian <harout.bezdjian@sbab.se>
2020-09-16 16:55:30 +0200

    Add env variable referencing to DB table

--- lambdaBucketEvent/index.js
+++ lambdaBucketEvent/index.js
@@ -1,48 +1,63 @@
 var AWS = require("aws-sdk");
 
-exports.handler = function(event, context, callback) {
-    console.log(event);
+exports.handler = function (event, context, callback) {
+  console.log(event);
 
-    var s3ObjectKey = event.Records[0].s3.object.key;
-    var s3ObjectSize = event.Records[0].s3.object.size;
-    var s3ObjectTime = event.Records[0].eventTime;
-    var s3BucketName = event.Records[0].s3.bucket.name;
+  var s3ObjectKey = event.Records[0].s3.object.key;
+  var s3ObjectSize = event.Records[0].s3.object.size;
+  var s3ObjectTime = event.Records[0].eventTime;
+  var s3BucketName = event.Records[0].s3.bucket.name;
 
-    console.log("Object Key: " + s3ObjectKey + " - Time: " + s3ObjectTime + " - BucketName: " + s3BucketName);
+  var ddbTable = process.env.DDB_TABLE;
 
-    // Create dynamo service object
-    var ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
-    // Create params with the values to save into the table.
-    var params = createDBParam(s3ObjectKey, s3ObjectTime,
-        s3ObjectSize.toString(), s3BucketName);
-    // Call Dynamo DB to add the items to the table.
-    ddb.putItem(params, function(error, data) {
-        if (error) {
-            callback(respond(500, "An error accured while adding an item" + error.message));
-        }
-        else {
-            callback(null, respond(200, "Successfully added " + s3ObjectKey + " into table"));
-        }
-    });
+  console.log("ddbTable: ", ddbTable);
+
+  console.log(
+      "Object Key: " + s3ObjectKey +
+      " - Time: " + s3ObjectTime +
+      " - BucketName: " + s3BucketName
+  );
+
+  // Create dynamo service object
+  var ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  // Create params with the values to save into the table.
+  var params = createDBParam(
+    ddbTable,
+    s3ObjectKey,
+    s3ObjectTime,
+    s3ObjectSize.toString(),
+    s3BucketName
+  );
+  // Call Dynamo DB to add the items to the table.
+  ddb.putItem(params, function (error, data) {
+    if (error) {
+      console.log("Error while puting item into table: ", error);
+      callback(Error(error));
+    } else {
+      var message = "Successfully added " + s3ObjectKey + " into table";
+      console.log(message);
+      callback(null, respond(200, message));
+    }
+  });
 };
 
-function createDBParam(key, time, size, bucketName) {
-    return {
-        TableName: 'cloud9HelloTable',
-        Item: {
-            'id': { S: key },
-            'timestamp': { S: time },
-            'size': { S: size },
-            'bucket_name': { S: bucketName }
-        }
-    };
+function createDBParam(ddbTable, key, time, size, bucketName) {
+  return {
+    TableName: ddbTable,
+    Item: {
+      id: { S: key },
+      timestamp: { S: time },
+      size: { S: size },
+      bucket_name: { S: bucketName },
+    },
+  };
 }
 
 function respond(status, message) {
-    return {
-        statusCode: status,
-        body: JSON.stringify({
-            message: message
-        }),
-    };
+  return {
+    statusCode: status,
+    body: JSON.stringify({
+      message: message,
+    }),
+  };
 }
