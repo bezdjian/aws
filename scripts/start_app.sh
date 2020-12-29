41b54a64fa63af41f4ea59049cd4ce6ea6a1960f
Harout Bezdjian <harout.bezdjian@sbab.se>
2020-12-30 00:20:25 +0100

    Add install java commands

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#sudo yum update -y
-#sudo yum install java-1.8.0-openjdk
+sudo yum update -y
+sudo yum install java-1.8.0-openjdk -y
 
-java -jar /home/ec2-user/aws-spring-0.1.jar &>/dev/null
+# Run the jar file in the background and log to aws-spring.log file whn still running.
+java -jar aws-spring-0.1.jar > aws-spring.log < /dev/null &
