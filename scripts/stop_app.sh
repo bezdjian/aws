2d3e7740fc18ba4805a0bd368f18f430a7bda5c8
Harout Bezdjian <harout.bezdjian@sbab.se>
2020-12-30 01:42:09 +0100

    Modify stop app script

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-# Get the PID of the task that runs 8080 and kill process.
-lsof -t -i tcp:8080 | xargs kill
+# Check if the port 8080 is active, then stop.
+if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
+    echo "App is running, stopping the app"
+    lsof -t -i tcp:8080 | xargs kill
+else
+    echo "App is not running.. continue."
+fi
