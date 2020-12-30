84d4f25b08bc31d2a8572d82c2b77ff908bb1680
Harout Bezdjian <harout.bezdjian@sbab.se>
2020-12-30 01:50:19 +0100

    Modify start app and remove stop app script

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,5 +3,14 @@
 sudo yum update -y
 sudo yum install java-1.8.0-openjdk -y
 
-# Run the jar file in the background and log to aws-spring.log file whn still running.
-java -jar aws-spring-0.1.jar > aws-spring.log < /dev/null &
+# Check if the port 8080 is active, then stop.
+if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
+    echo "Restarting the app"
+    lsof -t -i tcp:8080 | xargs kill
+    # Run the jar file in the background and log to aws-spring.log file whn still running.
+    java -jar aws-spring-0.1.jar > aws-spring.log < /dev/null &
+else
+    echo "Starting the app."
+    # Run the jar file in the background and log to aws-spring.log file whn still running.
+    java -jar aws-spring-0.1.jar > aws-spring.log < /dev/null &
+fi
--- scripts/stop_app.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Check if the port 8080 is active, then stop.
-if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
-    echo "App is running, stopping the app"
-    lsof -t -i tcp:8080 | xargs kill
-else
-    echo "App is not running.. continue."
-fi
