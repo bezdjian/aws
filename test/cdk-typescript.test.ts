8776822e65e72bca97cb34088e83c76cf414ae3a
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-05 16:47:12 +0200

    Move files and rename bin to src

--- test/cdk-typescript.test.ts
+++ test/cdk-typescript.test.ts
@@ -1,13 +1,17 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as CdkTypescript from '../lib/cdk-typescript-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as CdkTypescript from "../src/cdk-typescript-stack";
 
-test('Empty Stack', () => {
-    //const app = new cdk.App();
-    //// WHEN
-    //const stack = new CdkTypescript.CdkTypescriptStack(app, 'MyTestStack');
-    //// THEN
-    //expectCDK(stack).to(matchTemplate({
-    //  "Resources": {}
-    //}, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  //const app = new cdk.App();
+  //// WHEN
+  //const stack = new CdkTypescript.CdkTypescriptStack(app, 'MyTestStack');
+  //// THEN
+  //expectCDK(stack).to(matchTemplate({
+  //  "Resources": {}
+  //}, MatchStyle.EXACT))
 });
