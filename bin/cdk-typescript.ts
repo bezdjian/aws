12682c74eec30f0917e3fcb63891e9570279c157
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-05 09:44:35 +0200

    Add env property retrieving props from system env for accound and region.

--- bin/cdk-typescript.ts
+++ bin/cdk-typescript.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkTypescriptStack } from '../lib/cdk-typescript-stack';
+import {CdkTypescriptStack} from '../lib/cdk-typescript-stack';
 
 const app = new cdk.App();
 new CdkTypescriptStack(app, 'CdkTypescriptStack', {
@@ -11,11 +11,7 @@ new CdkTypescriptStack(app, 'CdkTypescriptStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-   env: { account: '397178420157', region: 'eu-north-1' },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
