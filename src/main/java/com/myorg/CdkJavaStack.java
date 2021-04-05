f35dc159ec8e3928620009f9620d78ccfd14b5e4
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-05 16:35:27 +0200

    Add Lambda function to the stack

--- src/main/java/com/myorg/CdkJavaStack.java
+++ src/main/java/com/myorg/CdkJavaStack.java
@@ -3,6 +3,12 @@ package com.myorg;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CdkJavaStack extends Stack {
     public CdkJavaStack(final Construct scope, final String id) {
@@ -11,5 +17,20 @@ public class CdkJavaStack extends Stack {
 
     public CdkJavaStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        // Build the Lambda function
+        Function function = Function.Builder.create(this, "CdkJavaHelloFunction")
+            .functionName("java-hello-cdk")
+            .handler("hello.handler")
+            .runtime(Runtime.NODEJS_10_X)
+            .code(Code.fromAsset("lambdafunction"))
+            .environment(createLambdaEnvVariables())
+            .build();
+    }
+
+    private Map<String, String> createLambdaEnvVariables() {
+        HashMap<String, String> lambdaEnvironment = new HashMap<>();
+        lambdaEnvironment.put("SOMEENV", "SOMEVAR");
+        return lambdaEnvironment;
     }
 }
