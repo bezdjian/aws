d86c97f68e41ce374cc074376004dd20bfcf54b9
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-05 16:36:33 +0200

    Add stack to the app

--- src/main/java/com/myorg/CdkJavaApp.java
+++ src/main/java/com/myorg/CdkJavaApp.java
@@ -1,11 +1,28 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
 
 public class CdkJavaApp {
 
     public static void main(final String[] args) {
         App app = new App();
+
+        new CdkJavaStack(app, "CdkJavaStack", StackProps.builder()
+            .description("Stack created by CDK with Java")
+            .env(buildEnvironment())
+            .stackName("CdkJavaStack")
+            .build());
+
         app.synth();
+    }
+
+    // Helper method to build an environment
+    private static Environment buildEnvironment() {
+        return Environment.builder()
+            .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+            .region(System.getenv("CDK_DEFAULT_REGION"))
+            .build();
     }
 }
