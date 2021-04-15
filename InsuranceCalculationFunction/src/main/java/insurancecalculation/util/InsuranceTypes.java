d74faa878ef69261f03365e98c08a14859b8e0d3
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-15 15:42:47 +0200

    Rename file and remove static methods and add constants for insurance types

---
+++ InsuranceCalculationFunction/src/main/java/insurancecalculation/util/InsuranceTypes.java
@@ -0,0 +1,9 @@
+package insurancecalculation.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class InsuranceTypes {
+    public final String INCOME_INSURANCE = "TRYGG";
+    public final String LIFE_INSURANCE = "LIV";
+}
