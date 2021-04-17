cfa0b3c00ff37386a98f9727380216f52cb4506e
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-17 11:16:49 +0200

    Remove unused InsuraneType util class

--- InsuranceCalculationFunction/src/main/java/insurancecalculation/util/InsuranceTypes.java
+++
@@ -1,9 +0,0 @@
-package insurancecalculation.util;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class InsuranceTypes {
-    public final String INCOME_INSURANCE = "TRYGG";
-    public final String LIFE_INSURANCE = "LIV";
-}
