c52f99affbca328de706f0f6f5e36ca14d689ddb
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-05-07 12:14:21 +0200

    Add Java lambda function

---
+++ lambdafunction/src/main/java/cdklambda/App.java
@@ -0,0 +1,36 @@
+package cdklambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        LambdaLogger logger = context.getLogger();
+
+        logger.log(input.toString());
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+            .withHeaders(getHeaders());
+        return response
+            .withHeaders(getHeaders())
+            .withStatusCode(200)
+            .withBody("hello!");
+    }
+
+    private Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        return headers;
+    }
+}
