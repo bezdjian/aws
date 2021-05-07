8cf715de8a48c8e61c5f0ef3e69309a835eed1db
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-05-07 17:02:23 +0200

    Add EC2 instance to the stack with security group

--- src/main/java/com/myorg/CdkJavaStack.java
+++ src/main/java/com/myorg/CdkJavaStack.java
@@ -1,5 +1,6 @@
 package com.myorg;
 
+import software.amazon.awscdk.core.CfnTag;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
@@ -8,9 +9,22 @@ import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.Table;
 import software.amazon.awscdk.services.dynamodb.TableProps;
+import software.amazon.awscdk.services.ec2.CfnInstance;
+import software.amazon.awscdk.services.ec2.CfnInstanceProps;
+import software.amazon.awscdk.services.ec2.IVpc;
+import software.amazon.awscdk.services.ec2.MachineImage;
+import software.amazon.awscdk.services.ec2.MachineImageConfig;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
+import software.amazon.awscdk.services.ec2.SecurityGroupProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcAttributes;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
+
+import java.util.Collections;
 
 public class CdkJavaStack extends Stack {
     public CdkJavaStack(final Construct scope, final String id) {
@@ -54,5 +68,37 @@ public class CdkJavaStack extends Stack {
 
         // Add GET method with lambda integration
         restApi.getRoot().addMethod("GET");
+
+        IVpc defaultVpc = Vpc.fromVpcAttributes(this, "VPC", VpcAttributes.builder()
+            .vpcId("vpc-4f569826")
+            .availabilityZones(Collections.singletonList("eu-north-1a"))
+            .build());
+
+        final Number sshPort = 22;
+        SecurityGroup ec2SecurityG = new SecurityGroup(this, "EC2SecurityG", SecurityGroupProps.builder()
+            .description("SG created by CDK for EC2InstanceForCdk")
+            .securityGroupName("EC2InstanceForCdk-SG")
+            .vpc(defaultVpc)
+            .allowAllOutbound(false)
+            .build());
+
+        // Add Inbound rules to SG
+        ec2SecurityG.addEgressRule(Peer.anyIpv4(), Port.tcp(sshPort), "Add SSH from anywhere");
+
+        // Get latest Amazon Linux machine image
+        MachineImageConfig latestAmazonLinuxImage = MachineImage.latestAmazonLinux().getImage(this);
+        // Create Name tag for EC2
+        CfnTag ec2Tag = CfnTag.builder()
+            .key("Name").value("EC2InstanceForCdk")
+            .build();
+
+        // Build EC2.. for testing
+        new CfnInstance(this, "EC2InstanceCdk", CfnInstanceProps.builder()
+            .imageId(latestAmazonLinuxImage.getImageId())
+            // .pem key name -> .keyName("Name")
+            .securityGroupIds(Collections.singletonList(ec2SecurityG.getSecurityGroupId()))
+            .instanceType("t3.micro")
+            .tags(Collections.singletonList(ec2Tag))
+            .build());
     }
 }
