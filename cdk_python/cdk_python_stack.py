cfe6fc4a3b1f3cd36f88d7bce1ccbbc4cb69081f
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-05-21 15:50:54 +0200

    Create Empty CDK stack

---
+++ cdk_python/cdk_python_stack.py
@@ -0,0 +1,7 @@
+from aws_cdk import core as cdk
+
+
+class CdkPythonStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
