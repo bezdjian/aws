a3db4945caea0929f56a60803378fed3b6213197
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-05-21 15:54:45 +0200

    Create and add Dynamo table

--- cdk_python/cdk_python_stack.py
+++ cdk_python/cdk_python_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import (core as cdk, aws_lambda, aws_apigateway)
+from aws_cdk import (core as cdk, aws_lambda, aws_apigateway, aws_dynamodb)
 
 
 class CdkPythonStack(cdk.Stack):
@@ -6,12 +6,27 @@ class CdkPythonStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Create Dynamo Table
+        table_name = 'cdkPythonTable'
+        table = aws_dynamodb.Table(self, table_name,
+                                   table_name=table_name,
+                                   partition_key=aws_dynamodb.Attribute(
+                                       name='id',
+                                       type=aws_dynamodb.AttributeType.STRING
+                                   ))
+
         # Create lambda function
         function = aws_lambda.Function(self, 'LambdaFunction',
                                        handler='function.handler',
                                        function_name='simple-function',
                                        runtime=aws_lambda.Runtime.PYTHON_3_8,
-                                       code=aws_lambda.Code.asset("./lambda"))
+                                       code=aws_lambda.Code.asset("./lambda"),
+                                       environment={
+                                           'DB_TABLE': table_name
+                                       })
+
+        # Grant function read on table
+        table.grant_read_data(function)
 
         # Create API Gateway
         api = aws_apigateway.LambdaRestApi(self, 'APIGateway',
--- lambda/function.py
+++ lambda/function.py
@@ -1,5 +1,11 @@
+import os
+
+
 def handler(event, context):
     print("event: ", event)
+
+    table_name = os.getenv('DB_TABLE')
+    print("Table: ", table_name)
 
     return {
         'StatusCode': 200,
