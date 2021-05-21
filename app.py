36d343d1599159ca471a90de56ec015e8127aa79
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-05-21 21:42:02 +0200

    Rename CdkPythonStack -> ServerlessStack

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import os
 
 from aws_cdk import core as cdk
-from cdk_python.cdk_python_stack import CdkPythonStack
-
+from cdk_python.cdk_python_stack import ServerlessStack
 
 app = cdk.App()
 
@@ -15,14 +14,14 @@ def get_env():
     )
 
 
-CdkPythonStack(app, "CdkPythonStack",
-               # If you don't specify 'env', this stack will be environment-agnostic.
-               # Account/Region-dependent features and context lookups will not work,
-               # but a single synthesized template can be deployed anywhere.
+ServerlessStack(app, "CdkPythonServerlessStack",
+                # If you don't specify 'env', this stack will be environment-agnostic.
+                # Account/Region-dependent features and context lookups will not work,
+                # but a single synthesized template can be deployed anywhere.
 
-               # Uncomment the next line to specialize this stack for the AWS Account
-               # and Region that are implied by the current CLI configuration.
-               # env=get_env()
-               )
+                # Uncomment the next line to specialize this stack for the AWS Account
+                # and Region that are implied by the current CLI configuration.
+                # env=get_env()
+                )
 
 app.synth()
--- cdk_python/cdk_python_stack.py
+++ cdk_python/cdk_python_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (core as cdk, aws_lambda, aws_apigateway, aws_dynamodb)
 
 
-class CdkPythonStack(cdk.Stack):
+class ServerlessStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
