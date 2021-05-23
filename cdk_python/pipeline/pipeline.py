44bcba4af449e077e10ff9946e5c5b7c7bc853e6
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-05-23 16:25:20 +0200

    Add comments to pipeline stack

--- cdk_python/pipeline/pipeline.py
+++ cdk_python/pipeline/pipeline.py
@@ -10,13 +10,15 @@ class PipelineStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        # Output artifact
         source_artifact = codepipeline.Artifact()
+        # Artifact to hold the cloudAssemblyArtifact for the synth action (the template)
         cloud_assembly_artifact = codepipeline.Artifact()
 
         pipeline = CdkPipeline(self, 'CdkPythonPipeline',
                                pipeline_name='CdkPythonPipeline',
                                cloud_assembly_artifact=cloud_assembly_artifact,
+                               # The Source stage
                                source_action=codepipeline_actions.GitHubSourceAction(
                                    action_name='GitHub',
                                    output=source_artifact,
@@ -25,6 +27,7 @@ class PipelineStack(cdk.Stack):
                                    owner='bezdjian',
                                    branch='cdk-python-lambda-api',
                                    repo='aws'),
+                               # Creates a Build stage
                                synth_action=SimpleSynthAction.standard_npm_synth(
                                    source_artifact=source_artifact,
                                    cloud_assembly_artifact=cloud_assembly_artifact,
@@ -32,5 +35,5 @@ class PipelineStack(cdk.Stack):
                                    build_command="pytest unittests",
                                    synth_command="cdk synth")
                                )
-        # Add serverless stack pre-prod deployment
+        # Add serverless stack to deployment with Pre-Prod stage
         pipeline.add_application_stage(ServerlessStackStage(self, 'Pre-Prod'))
