886020979d8092548418fb6c71ae47a0e1dd3e2b
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-06-09 13:31:57 +0200

    Remove pipeline stack

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
-import os
 
 from aws_cdk import core as cdk
-from cdk_python.pipeline.pipeline import PipelineStack
+from cdk_python.serverless_stack import ServerlessStack
 
 app = cdk.App()
 
+ServerlessStack(app, "CdkPythonServerlessStack",
+                # If you don't specify 'env', this stack will be environment-agnostic.
+                # Account/Region-dependent features and context lookups will not work,
+                # but a single synthesized template can be deployed anywhere.
 
-def get_env():
-    return cdk.Environment(
-        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region=os.getenv('CDK_DEFAULT_REGION')
-    )
-
-
-# ServerlessStack(app, "CdkPythonServerlessStack")
-PipelineStack(app, "CdkPythonPipelineStack",
-              # If you don't specify 'env', this stack will be environment-agnostic.
-              # Account/Region-dependent features and context lookups will not work,
-              # but a single synthesized template can be deployed anywhere.
-
-              # Uncomment the next line to specialize this stack for the AWS Account
-              # and Region that are implied by the current CLI configuration.
-              # env=get_env()
-              )
+                # Uncomment the next line to specialize this stack for the AWS Account
+                # and Region that are implied by the current CLI configuration.
+                # env=get_env()
+                )
 
 app.synth()
