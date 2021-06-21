3a91b6d8965df80a18aec1120869c6735fa9d439
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-06-21 11:19:11 +0200

    Update lambda with describe_stream and add tests and requirements

--- process_function/process.py
+++ process_function/process.py
@@ -1,13 +1,30 @@
 import json
 import os
+import boto3
+import logging
+from botocore.exceptions import ClientError
 
 
 def handler(event, context):
     table_name = os.getenv('DB_TABLE')
     stream_name = os.getenv('STREAM_NAME')
+    logger = logging.getLogger(__name__)
+    kinesis = boto3.client('kinesis')
 
     print("table_name: ", table_name)
     print("stream_name: ", stream_name)
+
+    try:
+        response = kinesis.describe_stream(StreamName=stream_name)
+        shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+        print("ShardId: ", shard_id)
+
+    except ClientError:
+        logger.exception("Couldn't get records from stream %s.", stream_name)
+        raise
+    except Exception as e:
+        logger.exception("Internal error %s.", e)
+        raise
 
     return {
         'statusCode': 200,
