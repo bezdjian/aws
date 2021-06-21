c5c9421f9b17f99eb9cddd7b547d6a24dfd2570e
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-06-21 17:23:37 +0200

    Enhance processor function to read data correctly and put in DynamoTable

--- data_processor_function/process.py
+++ data_processor_function/process.py
@@ -1,28 +1,57 @@
 import json
 import os
 import base64
+import boto3
+import uuid
+from botocore.exceptions import ClientError
 
 
 def handler(event, context):
     records = []
-    print("Kinesis Stream Event: ", json.dumps(event))
+    try:
+        for record in event["Records"]:
+            # Kinesis data is base64 encoded so decode here
+            decoded_data = base64 \
+                .b64decode(record["kinesis"]["data"]) \
+                .decode('utf-8')
+            # TODO, why double decodes to work?
+            payload = base64 \
+                .b64decode(decoded_data) \
+                .decode('utf-8')
 
+            payload = payload.replace('\'', '\"')
+            print("payload: ", payload)
+            record = json.loads(json.dumps(eval(payload)))
+            put_item(record["Model"], record["Speed"], record["Timestamp"])
+
+            records.append(payload)
+
+        return {
+            "StatusCode": 200,
+            "body": json.dumps({
+                "Data": records
+            })
+        }
+
+    except ClientError as e:
+        return {
+            "StatusCode": 500,
+            "body": json.dumps({
+                "message": e.response
+            })
+        }
+
+
+def put_item(model, speed, timestamp):
     table_name = os.getenv('DB_TABLE')
-    stream_name = os.getenv('STREAM_NAME')
-
     print("table_name: ", table_name)
-    print("stream_name: ", stream_name)
-
-    for record in event['Records']:
-        # Kinesis data is base64 encoded so decode here
-        payload = base64.b64decode(record["kinesis"]["data"])
-        decoded = str(payload)
-        print("Decoded payload: " + decoded)
-        records.append(decoded)
-
-    return {
-        "StatusCode": 200,
-        "body": json.dumps({
-            "Data": records
-        })
-    }
+    data_id = str(uuid.uuid4())
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table(table_name)
+    table.put_item(Item={
+        'id': data_id,
+        'model': model,
+        'speed': speed,
+        'timestamp': timestamp
+    })
