0d1a2a326de4c39931becf60bdbd9376eb24ab05
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-06-21 11:17:18 +0200

    Refactor commented out code, add files to gitignore

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from lambda_kinesis_stack.kinesis_serverless_stack import KinesisServerlessStack
 
 app = cdk.App()
 KinesisServerlessStack(app, "KinesisServerlessStack",
-                       # env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+                       # env=core.Environment(
+                       # account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+                       # region=os.getenv('CDK_DEFAULT_REGION')
+                       # ),
                        # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                        )
 
