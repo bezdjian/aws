1e1f18a11202c632c6b59474dd05a446f1066748
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-06-22 08:54:46 +0200

    Add logger to print

--- data_processor_function/process.py
+++ data_processor_function/process.py
@@ -3,11 +3,13 @@ import os
 import base64
 import boto3
 import uuid
+import logging
 from botocore.exceptions import ClientError
 
 
 def handler(event, context):
     records = []
+    logger = logging.getLogger(__name__)
     try:
         for record in event["Records"]:
             # Kinesis data is base64 encoded so decode here
@@ -20,7 +22,7 @@ def handler(event, context):
                 .decode('utf-8')
 
             payload = payload.replace('\'', '\"')
-            print("payload: ", payload)
+            logger.info("payload: ", payload)
             record = json.loads(json.dumps(eval(payload)))
             put_item(record["Model"], record["Speed"], record["Timestamp"])
 
@@ -44,9 +46,8 @@ def handler(event, context):
 
 def put_item(model, speed, timestamp):
     table_name = os.getenv('DB_TABLE')
-    print("table_name: ", table_name)
     data_id = str(uuid.uuid4())
-    
+
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table(table_name)
     table.put_item(Item={
--- data_producer_function/produce.py
+++ data_producer_function/produce.py
@@ -7,19 +7,19 @@ from botocore.exceptions import ClientError
 
 
 def handler(event, context):
-    print("API Event: ", event)
+    logger = logging.getLogger(__name__)
+    logger.info("API Event: ", event)
     body = event['body']
-    print("Event body: ", body)
+    logger.info("Event body: ", body)
 
     stream_name = os.getenv('STREAM_NAME')
 
-    logger = logging.getLogger(__name__)
     kinesis = boto3.client('kinesis', region_name='us-east-1')
 
-    print("data_payload: ", str(body))
-    print("Encode data...")
+    logger.info("data_payload: ", str(body))
+    logger.info("Encode data...")
     encoded_data = base64.b64encode(str.encode(str(body)))
-    print("Encoded data: ", encoded_data)
+    logger.info("Encoded data: ", encoded_data)
 
     try:
         response = kinesis.put_record(StreamName=stream_name,
