5031efc04486836e7db26ce044a557a69fa7e6b1
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-06-22 10:07:42 +0200

    Use logger for producer function

--- data_producer_function/produce.py
+++ data_producer_function/produce.py
@@ -5,21 +5,21 @@ import logging
 import base64
 from botocore.exceptions import ClientError
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 def handler(event, context):
-    print("API Event: ", event)
+    logger.info("API Event: %s", event)
     body = event['body']
-    print("Event body: ", body)
 
     stream_name = os.getenv('STREAM_NAME')
-
-    logger = logging.getLogger(__name__)
     kinesis = boto3.client('kinesis', region_name='us-east-1')
 
-    print("data_payload: ", str(body))
-    print("Encode data...")
+    logger.info("Event body: %s ", str(body))
+    logger.info("Encode data...")
     encoded_data = base64.b64encode(str.encode(str(body)))
-    print("Encoded data: ", encoded_data)
+    logger.info("Encoded data: %s", encoded_data)
 
     try:
         response = kinesis.put_record(StreamName=stream_name,
