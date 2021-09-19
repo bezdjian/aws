bca16798ee8d9cf0fd0046c2cedd6c06b68ceced
Harout Bezdjian <bezdjian-370z@hotmail.com>
2021-09-19 20:56:29 +0200

    Create items in HistoryTable when starting transaction

--- functions/start/start.py
+++ functions/start/start.py
@@ -16,6 +16,7 @@ def handler(event, context):
 
     state_machine_arn = os.environ.get("state_machine_arn")
     state_machine_name = os.environ.get("state_machine_name")
+    history_table = os.environ.get("history_table")
 
     test = bool(distutils.util.strtobool(
         os.environ.get("test")
@@ -30,8 +31,13 @@ def handler(event, context):
         product = body["product"]
 
         step_functions_client = boto3.client('stepfunctions')
+        dynamo_db = boto3.resource("dynamodb")
+        table = dynamo_db.Table(history_table)
+
+        transaction_id = str(uuid.uuid4())
 
         execution_input = {
+            "transaction_id": transaction_id,
             "transactionType": transaction_type,
             "product": product,
             "merchantId": merchant_id
@@ -41,10 +47,23 @@ def handler(event, context):
             logger.info("Running state machine %s", state_machine_name)
             step_functions_client.start_execution(
                 stateMachineArn=state_machine_arn,
-                name=str(uuid.uuid4()),
+                name=transaction_id,
                 input=json.dumps(execution_input),
                 traceHeader='string'
             )
+
+            response = table.put_item(
+                Item={
+                    'id': transaction_id,
+                    'transaction_type': transaction_type,
+                    'merchant_id': merchant_id,
+                    'information': {
+                        'product': product
+                    }
+                }
+            )
+
+            logger.info("Response from Table: %s", response)
         else:
             logger.info("Test is enabled. Skipping start of state machine %s", state_machine_name)
 
