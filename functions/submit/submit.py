24acfa7b09979139e56a2234655c5eaca9f6b45b
Harout Bezdjian <bezdjian-370z@hotmail.com>
2021-09-19 20:57:52 +0200

    Update HistoryTable items during submit transaction

--- functions/submit/submit.py
+++ functions/submit/submit.py
@@ -1,4 +1,8 @@
 import logging
+import os
+
+import boto3
+from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
@@ -7,29 +11,53 @@ logger.setLevel(level=logging.INFO)
 def handler(event, context):
     logger.info("Simple Function Event: %s", event)
 
-    transaction_type = event["transactionType"]
-    product = event["product"]
-    merchant_id = event["merchantId"]
+    try:
+        history_table = os.environ.get("history_table")
 
-    # Do something with input parameters...
+        transaction_type = event["transactionType"]
+        product = event["product"]
+        merchant_id = event["merchantId"]
+        transaction_id = event["transaction_id"]
 
-    logger.info("transaction_type: %s", transaction_type)
-    # Initiate response body
-    response_body = {
-        "merchantId": merchant_id
-    }
+        dynamo_db = boto3.resource("dynamodb")
+        table = dynamo_db.Table(history_table)
 
-    if transaction_type == "ORDER":
-        # code to put and prepare an order..
-        response_body["status"] = "ORDER"
-        response_body["price"] = "100"
-    elif transaction_type == "CANCEL":
-        # code to retrieve and cancel an order..
-        response_body["status"] = "CANCEL"
-        response_body["price"] = "0"
-    else:
-        response_body["status"] = "NONE"
+        logger.info("transaction_type: %s", transaction_type)
+        # Initiate response body
+        items = {
+            "merchantId": merchant_id
+        }
 
-    response = {"body": response_body}
-    logger.info("Returning %s", response)
-    return response
+        if transaction_type == "ORDER":
+            # code to put and prepare an order..
+            items["status"] = "ORDER"
+            items["price"] = "23995"
+            items["transaction_id"] = transaction_id
+            items["transaction_type"] = transaction_type
+            items["merchant_id"] = merchant_id
+            items["information"] = {
+                'product': product,
+                'price': '23995'
+            }
+            table_response = table.put_item(Item=items)
+            logger.info("Response from Table: %s", table_response)
+        elif transaction_type == "CANCEL":
+            # code to retrieve and cancel an order..
+            items["status"] = "CANCEL"
+            items["price"] = "0"
+        else:
+            items["status"] = "NONE"
+
+        response = {"body": items}
+        logger.info("Returning %s", response)
+        return response
+    except ClientError as c:
+        logger.exception(
+            "Error occurred: %s.", c)
+        raise
+    except KeyError as k:
+        logger.exception("Missing parameter! %s", k)
+        return {
+            "statusCode": "400",
+            "message": f"Missing parameter: {k}"
+        }
