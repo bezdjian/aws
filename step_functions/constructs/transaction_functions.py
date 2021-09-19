33571d5d733464bebfea926dd4c80a9b0a800a3c
Harout Bezdjian <bezdjian-370z@hotmail.com>
2021-09-19 09:40:51 +0200

    Add TransactionFunctions construct

---
+++ step_functions/constructs/transaction_functions.py
@@ -0,0 +1,55 @@
+from aws_cdk import (
+    core as cdk,
+    aws_lambda as lmb,
+    aws_apigateway as api)
+
+start_function = None
+submit_function = None
+order_function = None
+cancel_function = None
+
+
+class TransactionFunctions(cdk.Construct):
+    def __init__(self, scope: cdk.Construct, construct_id: str):
+        super().__init__(scope, construct_id)
+
+        self.start_function = lmb.Function(scope, "StartTransactionFunction",
+                                           code=lmb.Code.from_asset("functions/start"),
+                                           runtime=lmb.Runtime.PYTHON_3_9,
+                                           handler="start.handler",
+                                           function_name="StepFunction-StartTransaction")
+
+        self.submit_function = lmb.Function(scope, "SubmitTransactionFunction",
+                                            code=lmb.Code.from_asset("functions/submit"),
+                                            runtime=lmb.Runtime.PYTHON_3_9,
+                                            handler="submit.handler",
+                                            function_name="StepFunction-SubmitTransaction")
+
+        self.order_function = lmb.Function(scope, "OrderFunction",
+                                           code=lmb.Code.from_asset("functions/order"),
+                                           runtime=lmb.Runtime.PYTHON_3_9,
+                                           handler="order.handler",
+                                           function_name="StepFunction-Order")
+
+        self.cancel_function = lmb.Function(scope, "CancelFunction",
+                                            code=lmb.Code.from_asset("functions/cancel"),
+                                            runtime=lmb.Runtime.PYTHON_3_9,
+                                            handler="cancel.handler",
+                                            function_name="StepFunction-Cancel")
+
+        # API Gateway to trigger the start function
+        request_templates = {
+            "application/json": '{ "statusCode": "200" }'
+        }
+
+        sf_integration = api.LambdaIntegration(self.start_function,
+                                               request_templates=request_templates)
+        execution_api = api.RestApi(scope, "StartExecutionAPI",
+                                    rest_api_name="StartExecutionAPI")
+
+        execution_api.root.add_resource("execute").add_method("POST", sf_integration)
+
+        # Output API Gateways url
+        cdk.CfnOutput(scope, "StartExecutionAPI-URL",
+                      export_name="StartExecutionAPI-URL",
+                      value=execution_api.url)
--- step_functions/step_functions_stack.py
+++ step_functions/step_functions_stack.py
@@ -1,10 +1,10 @@
 from aws_cdk import (
     core as cdk,
-    aws_lambda as lmb,
     aws_stepfunctions as sfn,
     aws_stepfunctions_tasks as sf_task,
-    aws_apigateway as api,
 )
+
+from step_functions.constructs.transaction_functions import TransactionFunctions
 
 
 class StepFunctionsStack(cdk.Stack):
@@ -12,53 +12,24 @@ class StepFunctionsStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Lambda function triggered by API Gateway to read the input, do some logic and start the state machine.
-        start_function = lmb.Function(self, "StartTransactionFunction",
-                                      code=lmb.Code.from_asset("function/start"),
-                                      runtime=lmb.Runtime.PYTHON_3_8,
-                                      handler="start.handler",
-                                      function_name="StepFunction-StartTransaction")
+        transaction_functions = TransactionFunctions(self, "TransactionFunctions")
 
-        # Lambda function that reads the input from start_function and submits
-        submit_function = lmb.Function(self, "SubmitTransactionFunction",
-                                       code=lmb.Code.from_asset("function/submit"),
-                                       runtime=lmb.Runtime.PYTHON_3_8,
-                                       handler="submit.handler",
-                                       function_name="StepFunction-SubmitTransaction")
-
-        # Lambda function that processes the order if the step function's transactionType = "ORDER"
-        order_function = lmb.Function(self, "OrderFunction",
-                                      code=lmb.Code.from_asset("function/order"),
-                                      runtime=lmb.Runtime.PYTHON_3_8,
-                                      handler="order.handler",
-                                      function_name="StepFunction-Order")
-
-        # Lambda function that processes the order if the step function's transactionType = "CANCEL"
-        cancel_function = lmb.Function(self, "CancelFunction",
-                                       code=lmb.Code.from_asset("function/cancel"),
-                                       runtime=lmb.Runtime.PYTHON_3_8,
-                                       handler="cancel.handler",
-                                       function_name="StepFunction-Cancel")
-
-        # Lambda task for submit function
-        submit_transaction = sf_task.RunLambdaTask(lambda_function=submit_function,
+        submit_transaction = sf_task.RunLambdaTask(lambda_function=transaction_functions.submit_function,
                                                    payload=sfn.TaskInput.from_data_at("$"))
-        # Lambda task for order function
-        order_transaction = sf_task.RunLambdaTask(lambda_function=order_function,
+
+        order_transaction = sf_task.RunLambdaTask(lambda_function=transaction_functions.order_function,
                                                   payload=sfn.TaskInput.from_data_at("$"))
-        # Lambda task for cancel function
-        cancel_transaction = sf_task.RunLambdaTask(lambda_function=cancel_function,
+
+        cancel_transaction = sf_task.RunLambdaTask(lambda_function=transaction_functions.cancel_function,
                                                    payload=sfn.TaskInput.from_data_at("$"))
-        # State machine Task for submit function
+
         submit_job_task = sfn.Task(self, "SubmitTransactionTask", task=submit_transaction,
                                    # Send output starting from Payload, which makes it look like
                                    # {body: status: ORDER}
                                    output_path="$.Payload")
-        # State machine Task for order function
         order_job_task = sfn.Task(self, "OrderTransactionTask", task=order_transaction,
                                   # Send output starting from Payload
                                   output_path="$.Payload")
-        # State machine Task for cancel function
         cancel_job_task = sfn.Task(self, "CancelTransactionTask", task=cancel_transaction,
                                    # Send output starting from Payload
                                    output_path="$.Payload")
@@ -75,7 +46,6 @@ class StepFunctionsStack(cdk.Stack):
                                     error="Transaction error",
                                     cause="Error occurred while processing transaction")
 
-        # State machine Choice for order
         order_status = sfn.Choice(self, "OrderStatus") \
             .when(sfn.Condition.string_equals("$", "COMPLETED"), succeed) \
             .when(sfn.Condition.string_equals("$.body.status", "ERROR"), transaction_fail)
@@ -97,28 +67,15 @@ class StepFunctionsStack(cdk.Stack):
                                          definition=definition,
                                          state_machine_name="StateMachine-SimpleResult")
 
-        start_function.add_environment("state_machine_arn", state_machine.state_machine_arn)
-        start_function.add_environment("state_machine_name", state_machine.state_machine_name)
+        transaction_functions.start_function.add_environment("state_machine_arn", state_machine.state_machine_arn)
+        transaction_functions.start_function.add_environment("state_machine_name", state_machine.state_machine_name)
 
         # Grant start_function to execute state machine
-        state_machine.grant_start_execution(start_function)
-
-        request_templates = {
-            "application/json": '{ "statusCode": "200" }'
-        }
-        sf_integration = api.LambdaIntegration(start_function, request_templates=request_templates)
-        execution_api = api.RestApi(self, "StartExecutionAPI",
-                                    rest_api_name="StartExecutionAPI")
-
-        execution_api.root.add_resource("execute").add_method("POST", sf_integration)
+        state_machine.grant_start_execution(transaction_functions.start_function)
 
         cdk.CfnOutput(self, "StateMachineName",
                       export_name="StateMachineName",
                       value=state_machine.state_machine_name)
-
-        cdk.CfnOutput(self, "StartExecutionAPI-URL",
-                      export_name="StartExecutionAPI-URL",
-                      value=execution_api.url)
 
         # start_execution = sf_task.StepFunctionsStartExecution(self, "StartStepFunctionExecution",
         #                                                       state_machine=state_machine,
