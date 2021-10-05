44a748fd57741663a8ac0029da2352b1f8f387d8
SHBMAIN\habe10 <habe10@handelsbanken.se>
2021-10-05 15:59:29 +0200

    Add event for order function and json load event body

--- functions/order/order.py
+++ functions/order/order.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
@@ -7,15 +8,14 @@ logger.setLevel(level=logging.INFO)
 def handler(event, context):
     logger.info("Result Function Event: %s", event)
 
-    status = "NONE"
-
     try:
-        input_body = event["body"]
+        input_body = json.loads(event["body"])
         logger.info("input_body: %s", input_body)
 
         input_status = input_body["status"]
         input_price = input_body["price"]
         logger.info("input_status: %s", input_status)
+        logger.info("input_price: %s", input_price)
         # Complete the order
         # code ...
         status = "COMPLETED"
