d29c4b6aabb67308dc861a5a0342cce718400ba3
Harout Bezdjian <bezdjian-370z@hotmail.com>
2021-10-10 20:24:27 +0200

    Refactor App Stack

--- src/main/java/com/myorg/CdkJavaApp.java
+++ src/main/java/com/myorg/CdkJavaApp.java
@@ -1,6 +1,5 @@
 package com.myorg;
 
-import org.jetbrains.annotations.NotNull;
 import software.amazon.awscdk.core.App;
 import software.amazon.awscdk.core.Environment;
 import software.amazon.awscdk.core.StackProps;
@@ -9,12 +8,13 @@ public class CdkJavaApp {
 
     public static void main(final String[] args) {
         App app = new App();
-        CdkJavaServerlessStack serverlessStack = new CdkJavaServerlessStack(app, "CdkJavaStack", getCdkStackProps());
+        CdkJavaServerlessStack serverlessStack = new CdkJavaServerlessStack(app, 
+                "CdkJavaStack", 
+                getCdkStackProps());
         new CdkJavaEC2Stack(serverlessStack);
         app.synth();
     }
 
-    @NotNull
     private static StackProps getCdkStackProps() {
         return StackProps.builder()
             .description("Stack created by CDK with Java")
@@ -23,7 +23,6 @@ public class CdkJavaApp {
             .build();
     }
 
-    // Helper method to build an environment
     private static Environment buildEnvironment() {
         return Environment.builder()
             .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
