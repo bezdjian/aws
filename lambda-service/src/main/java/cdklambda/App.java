b5313a9c2b270622963f4d899b76b406270d2e44
Harout Bezdjian <bezdjian-370z@hotmail.com>
2021-10-11 13:42:01 +0200

    Refactor lambda service and comment out SsmParameter request
    SsmParameter request taking time, test it after deploying to AWS.

--- lambda-service/src/main/java/cdklambda/App.java
+++ lambda-service/src/main/java/cdklambda/App.java
@@ -3,7 +3,6 @@ package cdklambda;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -11,12 +10,15 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.http.HttpStatus;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
 import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,50 +31,57 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         try {
             LambdaLogger logger = context.getLogger();
 
-            logger.log("\n\n Input: " + input.toString());
+            logInputValues(input, logger);
 
             String dynamoTableName = System.getenv("DB_TABLE");
-            String ssmParamName = System.getenv("SSM_PARAM_NAME");
+            //String ssmParamName = System.getenv("SSM_PARAM_NAME");
+            //final SsmClient ssmClient = getSsmClient();
+            //GetParameterResponse parameter = getParameterResponse(ssmClient, ssmParamName);
+            //logger.log("\n Parameter value: " + parameter.parameter().value() + "\n\n");
+            //ssmClient.close();
 
-            logger.log("\n dynamoTableName: " + dynamoTableName);
-            logger.log("\n ssmParamName: " + ssmParamName);
-
-            final SsmClient ssmClient = getSsmClient();
-            GetParameterResponse parameter = getParameterResponse(ssmClient, ssmParamName);
-            logger.log("\n Parameter value: " + parameter.parameter().value() + "\n\n");
-            ssmClient.close();
-
+            List<String> response = new ArrayList<>();
             ItemCollection<ScanOutcome> items = scanDynamoDB(dynamoTableName);
 
-            for (Item item : items) {
-                logger.log("\n Item: " + item.toString());
-            }
+            items.spliterator()
+                    .forEachRemaining(item -> {
+                        logger.log("\n Item: " + item.toJSON());
+                        response.add(item.toJSON());
+                    });
 
             logger.log("\n\n");
-            return gatewayResponse(200, "Hello!");
+            return gatewayResponse(HttpStatus.SC_OK, response.toString());
         } catch (Exception e) {
-            return gatewayResponse(500, e.getMessage());
+            return gatewayResponse(HttpStatus.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 
+    private void logInputValues(APIGatewayProxyRequestEvent input, LambdaLogger logger) {
+        logger.log("\n\n Input: " + input.toString());
+        logger.log("\n\n Input body: " + input.getBody());
+    }
+
     private ItemCollection<ScanOutcome> scanDynamoDB(String dynamoTableName) {
+        return createDynamoDBClient()
+                .getTable(dynamoTableName)
+                .scan();
+    }
+
+    private DynamoDB createDynamoDBClient() {
         AmazonDynamoDB client = AmazonDynamoDBClient.builder().build();
-        DynamoDB dynamoDB = new DynamoDB(client);
-        return dynamoDB
-            .getTable(dynamoTableName)
-            .scan();
+        return new DynamoDB(client);
     }
 
     private SsmClient getSsmClient() {
         return SsmClient.builder()
-            .region(Region.EU_NORTH_1)
-            .build();
+                .region(Region.EU_NORTH_1)
+                .build();
     }
 
     private GetParameterResponse getParameterResponse(SsmClient ssmClient, String ssmParamName) {
         GetParameterRequest parameterRequest = GetParameterRequest.builder()
-            .name(ssmParamName)
-            .build();
+                .name(ssmParamName)
+                .build();
 
         return ssmClient.getParameter(parameterRequest);
     }
@@ -83,8 +92,8 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         headers.put("X-Custom-Header", "application/json");
 
         return new APIGatewayProxyResponseEvent()
-            .withHeaders(headers)
-            .withStatusCode(statusCode)
-            .withBody(message);
+                .withHeaders(headers)
+                .withStatusCode(statusCode)
+                .withBody(message);
     }
 }
