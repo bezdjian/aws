9e23f0e238d9878c9bd2aa1041c6372390b87d8b
bezdjian <bezdjian-370z@hotmail.com>
2022-06-06 11:23:48 +0200

    Add info about awsls in commands.sh

--- commands.sh
+++ commands.sh
@@ -2,6 +2,8 @@
 pip install localstack
 localstack start -d
 
+# OBS: awsls is an alias to 'aws --endpoint-url=http://localhost:4566', awslocal cli was not working as intended.
+
 # Create Dynamo table
 awsls dynamodb create-table --table-name sns-message-table --attribute-definitions AttributeName="id",AttributeType="S" --key-schema AttributeName="id",KeyType="HASH" --billing-mode PAY_PER_REQUEST
 
