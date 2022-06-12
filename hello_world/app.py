2ce055511a18486509a50af3ddeac3545f40e821
bezdjian <bezdjian-370z@hotmail.com>
2022-06-12 16:34:23 +0200

    Edit comment on using boto3.client

--- hello_world/app.py
+++ hello_world/app.py
@@ -11,11 +11,12 @@ def lambda_handler(event, context):
 
         if env == "dev":
             print("it is dev")
-            # not working on Linux, only Mac as of now:
-            s3 = boto3.client("s3", endpoint_url="http://host.docker.internal:4566")
-            # Works on Linux, need to add ´--docker-network sam_localstack_network´
+            # 1: Does not work on Linux, only Mac as of now:
+            # s3 = boto3.client("s3", endpoint_url="http://host.docker.internal:4566")
+
+            # 2: Works on Linux (also Mac), need to add ´--docker-network sam_localstack_network´
             # localstack is the name of the service in docker-compose
-            # s3 = boto3.client("s3", endpoint_url="http://localstack:4566")
+            s3 = boto3.client("s3", endpoint_url="http://localstack:4566")
         else:
             print("it is not dev")
             s3 = boto3.client("s3")
