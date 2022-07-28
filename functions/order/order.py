57f4eebbbb95452469981495790ae9307b8bfd14
Harout Bezdjian <bezdjian-370z@hotmail.com>
2022-07-28 19:46:32 +0200

    Remove json.loads since the event body is already a string JSON in order function

--- functions/order/order.py
+++ functions/order/order.py
@@ -1,19 +1,18 @@
 import logging
-import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 
 
 def handler(event, context):
-    logger.info("Result Function Event: %s", event)
+    logger.info("Order function event: %s", event)
 
     try:
-        input_body = json.loads(event["body"])
-        logger.info("input_body: %s", input_body)
+        event_body = event["body"]
+        logger.info("event_body: %s", event_body)
 
-        input_status = input_body["status"]
-        input_price = input_body["price"]
+        input_status = event_body["status"]
+        input_price = event_body["price"]
         logger.info("input_status: %s", input_status)
         logger.info("input_price: %s", input_price)
         # Complete the order
