4e20dc01ad076529dd5790610edd399eb9f528d3
bezdjian <bezdjian-370z@hotmail.com>
2022-07-31 20:01:01 +0200

    wip: unit test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,73 +1,56 @@
 import json
-
+import os
 import pytest
 
-from hello_world import app
+from receive_message import app
+from moto import mock_dynamodb
 
 
 @pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+def sns_event():
+    """ Generates SNS Event"""
 
     return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
+        "Records": [
+            {
+                "EventSource": "aws:sns",
+                "EventVersion": "1.0",
+                "EventSubscriptionArn": "arn:aws:sns:us-east-1::ExampleTopic",
+                "Sns": {
+                    "Type": "Notification",
+                    "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+                    "TopicArn": "arn:aws:sns:eu-north-1:000000000000:sam-topic-test",
+                    "Subject": "example subject",
+                    "Message": "Test message from event/sns_event.json",
+                    "Timestamp": "2022-03-01T10:23:44.000Z",
+                    "SignatureVersion": "1",
+                    "Signature": "EXAMPLE",
+                    "SigningCertUrl": "EXAMPLE",
+                    "UnsubscribeUrl": "EXAMPLE",
+                    "MessageAttributes": {
+                        "Test": {
+                            "Type": "String",
+                            "Value": "TestString"
+                        },
+                        "TestBinary": {
+                            "Type": "Binary",
+                            "Value": "TestBinary"
+                        }
+                    }
+                }
+            }
+        ]
     }
 
 
-def test_lambda_handler(apigw_event, mocker):
+@mock_dynamodb
+def test_lambda_handler(sns_event):
+    os.environ.setdefault("DB_TABLE", "table")
+    # os.environ.setdefault("LOCAL_STACK_ENDPOINT", "end")
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
+    lambda_result = app.lambda_handler(sns_event, "")
+    data = json.loads(lambda_result["body"])
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
+    assert lambda_result["statusCode"] == 200
+    assert "message" in lambda_result["body"]
     assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
