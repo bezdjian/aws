8594c3fea3be40a82bb89327e38f58c62b2f89e9
bezdjian <bezdjian-370z@hotmail.com>
2022-12-27 14:46:08 +0100

    Refactor BucketController and pom

--- src/main/java/com/example/demo/BucketController.java
+++ src/main/java/com/example/demo/BucketController.java
@@ -5,47 +5,57 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import com.example.demo.model.LambdaResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.LambdaResponse;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class BucketController {
 
-  @GetMapping
+  @GetMapping("/buckets")
   public LambdaResponse listBuckets() {
-    AmazonS3ClientBuilder s3ClientBuilder = AmazonS3ClientBuilder.standard();
+    AmazonS3 s3 = getAmazonS3();
 
-    Optional.ofNullable(System.getenv("LOCALSTACK"))
-        .ifPresent(endpoint -> setEndpointConfiguration(s3ClientBuilder, endpoint));
-
-    AmazonS3 s3 = s3ClientBuilder.build();
-
-    System.out.println("--- Got s3 client: " + s3.getRegionName());
+    log.info("*** Got s3 client in region: {}", s3.getRegionName());
 
     List<Bucket> buckets = s3.listBuckets();
-    System.out.println("Your Amazon S3 buckets are:");
-    for (Bucket b : buckets) {
-      System.out.println("* " + b.getName());
-    }
+    log.info("*** Your Amazon S3 buckets are:");
+    buckets.forEach(b -> log.info("*** {}", b.getName()));
 
-     List<String> bucketNames = s3.listBuckets().stream()
-        .map(Bucket::getName)
-        .collect(Collectors.toList());
+    List<String> bucketNames = getBucketNames(s3);
 
     return new LambdaResponse(bucketNames);
   }
 
+  private static List<String> getBucketNames(AmazonS3 s3) {
+    return s3.listBuckets().stream()
+      .map(Bucket::getName)
+      .collect(Collectors.toList());
+  }
+
+  private AmazonS3 getAmazonS3() {
+    AmazonS3ClientBuilder s3ClientBuilder = AmazonS3ClientBuilder.standard();
+
+    Optional.ofNullable(System.getenv("LOCALSTACK"))
+      .ifPresentOrElse(endpoint -> setEndpointConfiguration(s3ClientBuilder, endpoint),
+        () -> s3ClientBuilder.withRegion(Regions.EU_NORTH_1));
+
+    return s3ClientBuilder.build();
+  }
+
   private void setEndpointConfiguration(AmazonS3ClientBuilder s3ClientBuilder, String endpoint) {
+    log.info("*** LOCALSTACK is present, setting endpoint: {}", endpoint);
     s3ClientBuilder.setEndpointConfiguration(
         new AwsClientBuilder.EndpointConfiguration("http://" + endpoint + ":4566",
-            Regions.EU_WEST_1.getName())
+            Regions.EU_NORTH_1.getName())
     );
   }
 }
