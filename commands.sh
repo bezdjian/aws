9db3043413dfaa5e419dfaec0f2c7271b2358860
Harout Bezdjian <harout.bezdjian@wirelesscar.com>
2023-05-22 22:21:29 +0200

    Edit commands.sh to work with localStack

--- commands.sh
+++ commands.sh
@@ -1,26 +1,19 @@
 # Install & run localstack
-#pip install localstack
+# pip install localstack
 localstack start -d
 
-# OBS: awsls is an alias to 'aws --endpoint-url=http://localhost:4566', awslocal cli was not working as intended.
+# create bucket 'buckets4lambda' for sam to upload the project.
+awslocal s3 mb s3://buckets4lambda
 
-## OBS: Provision template.yaml to localStack instead of running the manual commands below!
-
-# Create Dynamo table
-awsls dynamodb create-table --table-name sns-message-table --attribute-definitions AttributeName="id",AttributeType="S" --key-schema AttributeName="id",KeyType="HASH" --billing-mode PAY_PER_REQUEST
-
-# Create topic, copy the ARN
-awsls sns create-topic --name sam-topic-test
-
-# Lambda subscription
-awsls sns subscribe --topic-arn arn:aws:sns:eu-north-1:000000000000:sam-topic-test --protocol lambda --notification-endpoint http://localhost:4566/000000000000/sam-queue-test
+# pip install aws-sam-cli-local
+samlocal deploy
 
 # Run with parameters and local env
 # sam local invoke --env-vars local.json --parameter-overrides=ParameterKey=environment,ParameterValue=prod
-sam local invoke --event events/sns_event.json --env-vars local.json
+samlocal local invoke --event events/sns_event.json --env-vars local.json
 
 # Scan table
-awsls dynamodb scan --table-name sns-message-table
+awslocal dynamodb scan --table-name sns-message-table
 
 #Scan table - get count only
-awsls dynamodb scan --table-name sns-message-table --query Count
+awslocal dynamodb scan --table-name sns-message-table --query Count
