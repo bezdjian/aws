6b22e378eee5618248e8e4bc8ec4229789933e25
Harout Bezdjian <harout.bezdjian@cabonline.com>
2025-12-22 09:30:53 +0100

    feat: Refactor CalculationService to export individual functions and add token verification

--- frontend/src/backend/service.jsx
+++ frontend/src/backend/service.jsx
@@ -1,27 +1,47 @@
 import axios from "axios";
 
-class CalculationService {
-    static BASE_URL = import.meta.env.VITE_BACKEND_API_URL || "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_BACKEND_API_URL || "http://localhost:8000";
 
-    static getAllCalculations() {
-        return axios.get(`${this.BASE_URL}/calculations`);
-    }
 
-    static getCalculationById(calculation_id) {
-        return axios.get(`${this.BASE_URL}/calculations/${calculation_id}`);
-    }
-
-    static createCalculation(calculation) {
-        return axios.post(`${this.BASE_URL}/calculations`, calculation);
-    }
-
-    static updateCalculation(id, calculation) {
-        return axios.put(`${this.BASE_URL}/calculations/${id}`, calculation);
-    }
-
-    static deleteCalculation(id) {
-        return axios.delete(`${this.BASE_URL}/calculations/${id}`);
+export const verifyToken = async (token) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/auth/verify`, {
+            token: token,
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error validating token:", error);
+        throw error;
     }
 }
 
-export default CalculationService;
+
+export const getClientId = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/auth/client_id`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching client ID:", error);
+        throw error;
+    }
+}
+
+export const getAllCalculations = () => {
+    return axios.get(`${BASE_URL}/calculations`);
+}
+
+export const getCalculationById = (calculation_id) => {
+    return axios.get(`${BASE_URL}/calculations/${calculation_id}`);
+}
+
+export const createCalculation = (calculation) => {
+    return axios.post(`${BASE_URL}/calculations`, calculation);
+}
+
+export const updateCalculation = (id, calculation) => {
+    return axios.put(`${BASE_URL}/calculations/${id}`, calculation);
+}
+
+export const deleteCalculation = (id) => {
+    return axios.delete(`${BASE_URL}/calculations/${id}`);
+}
